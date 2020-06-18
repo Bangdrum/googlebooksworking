5c04881bb19464b885b0af339772d3a61771d779
Ryan Shepard <bangdrumshep@gmail.com>
2020-06-17 21:05:57 -0700

    created build for heroku

---
+++ client/build/precache-manifest.820084eb1a05c272a2084b78cf8c3b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da871235d837d03d2409",
+    "url": "/static/css/main.bb8b8123.chunk.css"
+  },
+  {
+    "revision": "da871235d837d03d2409",
+    "url": "/static/js/main.d71bd014.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "354271c92ef98bedf194",
+    "url": "/static/css/2.3729fd53.chunk.css"
+  },
+  {
+    "revision": "354271c92ef98bedf194",
+    "url": "/static/js/2.8f9941c6.chunk.js"
+  },
+  {
+    "revision": "61adab2adf00358c8d13fbab14a02f0d",
+    "url": "/index.html"
+  }
+];
